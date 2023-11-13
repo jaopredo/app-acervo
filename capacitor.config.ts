5ba592120ccce2c47408e7a85b8cffe6effb5e8f
jaopredo <joao.profissional.45@gmail.com>
2023-11-13 14:15:28 -0300

    Primeiro Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.acervo.android',
+  appName: 'app-acervo',
+  webDir: 'out',
+  server: {
+    androidScheme: 'https',
+    url: 'http://192.168.1.200',
+    cleartext: true
+  }
+};
+
+export default config;
