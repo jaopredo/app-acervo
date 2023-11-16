7b86d6180f3b2df61f5991617ca44f371d1de0e7
jaopredo <joao.profissional.45@gmail.com>
2023-11-16 13:28:57 -0300

    Armazenamento Local feito

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,5 +1,9 @@
 'use client'
+import { useState } from "react"
 import { useForm, SubmitHandler } from "react-hook-form"
+
+/* STORAGE */
+import LocalStorage from "@/storage"
 
 /* CONTEXTS */
 import { useGlobalContext } from "@/contexts"
@@ -8,12 +12,15 @@ import { useGlobalContext } from "@/contexts"
 import { UserLogin } from "@/types/components/user"
 
 export default function Page() {
+    const [ local, setLocal ] = useState<any>()
+
     const { register, handleSubmit } = useForm<UserLogin>()
     const { userService } = useGlobalContext()
 
     const onSubmit: SubmitHandler<UserLogin> = credentials => {
         async function login() {
             const resp = await userService.login(credentials)
+            LocalStorage.save('token', resp.authorisation.token)
         }
         login()
     }
@@ -31,5 +38,9 @@ export default function Page() {
             </div>
             <button>LOGIN</button>
         </form>
+        <div>
+            <button onClick={() => setLocal(LocalStorage.get('token'))}>TESTE</button>
+            <p>{local}</p>
+        </div>
     </div>
 }
