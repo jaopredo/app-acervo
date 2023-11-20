8c7719fcf572bb24d3282472516262d24649ddf0
jaopredo <joao.profissional.45@gmail.com>
2023-11-20 15:19:13 -0300

    Loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '@/scss/global.scss'
-// import { Head } from 'next/document'
 
 import { GlobalContextProvider } from '../contexts/global'
 
